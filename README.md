7e1a5d630638394cedd2165021213eb0a9077403
Surukati Asutosh Dora <78288601+SurukatiAsutoshDora@users.noreply.github.com>
2022-06-29 18:43:27 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# test
+# Todolist_React
+ 
---
+++ build/precache-manifest.f04fc522846f89f5b89f7583efc1d78d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f4fed261a175fbb97febdad61f69e35",
+    "url": "/Todolist/index.html"
+  },
+  {
+    "revision": "5a6035e60db09faea70f",
+    "url": "/Todolist/static/js/2.3c779e7e.chunk.js"
+  },
+  {
+    "revision": "68ff926225a96e53ee6d",
+    "url": "/Todolist/static/js/main.b21c4566.chunk.js"
+  },
+  {
+    "revision": "5b44b01edb02ea7e55fa",
+    "url": "/Todolist/static/js/runtime-main.b2de9725.js"
+  }
+]);
